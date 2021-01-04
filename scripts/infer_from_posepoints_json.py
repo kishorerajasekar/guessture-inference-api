adcf363b6158a308ede62bdab419b2006cbc2727
Asapanna Rakesh <rakeshark22@gmail.com>
2021-01-04 12:59:52 +0530

    debug

--- scripts/infer_from_posepoints_json.py
+++ scripts/infer_from_posepoints_json.py
@@ -120,4 +120,3 @@ if __name__ == "__main__":
             for frame_num in range(len(processor)):
                 ret = processor.predict(frame_num)
                 print(f"frame{frame_num} \t:{ret}")
-                print("--------------------------------------------------------------")
